37dfaaadc9a696c152553b492da5c2e18203b6be
Ramzan <forjobmrtz@gmail.com>
2022-12-24 02:07:36 +0300

    feat: HomeWork 7 is done

--- HomeWork_007/#001/Program.cs
+++ HomeWork_007/#001/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int m = 3;
+int n = 4;
+double[,] arr1 = new double[m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = n-1; j >= 0; j--)
+    {
+        Random rnd = new Random();
+        arr1[i, j] = Math.Round(rnd.NextDouble() * 10, 2);
+        Console.Write(arr1[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+
+
+
+
+/*
+Задача 1. Задайте двумерный массив размером m×n,
+заполненный случайными вещественными числами.
+Тип данных для элементов выбрать double, наличие ненулевой дробной части обязательно.
+m = 3, n = 4.
+0.5 7.96 -2.78 -0.2
+1.7 -3.3 8.574 -9.9
+8.5 7.87 -7.1 9.15
+*/
---
+++ HomeWork_007/#002/Program.cs
@@ -0,0 +1,39 @@
+﻿int m = 3;
+int n = 4;
+double[,] arr1 = new double[m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = n - 1; j >= 0; j--)
+    {
+        Random rnd = new Random();
+        arr1[i, j] = Math.Round(rnd.NextDouble() * 10, 2);
+        Console.Write(arr1[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+Console.WriteLine("Введите номер строки: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца: ");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x >= arr1.GetLength(0) || y >= arr1.GetLength(1)) Console.WriteLine("Такого элемента нет в списке");
+else Console.WriteLine("Запрошенный элемент массива: " + arr1[x, y]);
+
+
+
+
+
+/*
+Задача 2. Напишите программу, которая на вход принимает позиции (два индекса) элемента в двумерном массиве,
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+[1,7] -> такого числа в массиве нет
+*/
