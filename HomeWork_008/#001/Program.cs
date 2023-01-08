2ee3b70331a32a53603cae6ff1779d42bc97dd31
Ramzan <forjobmrtz@gmail.com>
2023-01-09 01:24:25 +0300

    feat: HomeWork 8 is done

---
+++ HomeWork_008/#001/Program.cs
@@ -0,0 +1,52 @@
+﻿Console.WriteLine("Задан массив из случайных чисел: ");
+int[,] arr1 = new int[3, 4];
+int[] arr2 = new int[4];
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 4 - 1; j >= 0; j--)
+    {
+        Random rnd = new Random();
+        arr1[i, j] = rnd.Next(1, 10);
+        Console.Write(arr1[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Упорядоченные строки массива по убыванию элементов строки: ");
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        arr2[j] = arr1[i, j];
+    }
+
+    Array.Sort(arr2);
+    Array.Reverse(arr2);
+
+    for (int k = 0; k < 4; k++)
+    {
+        arr1[i, k] = arr2[k];
+        Console.Write(arr1[i, k] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+
+
+
+
+
+
+
+/*
+Задача 1: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
---
+++ HomeWork_008/#003/Program.cs
@@ -0,0 +1,58 @@
+﻿Console.WriteLine("Заданы два массива из случайных чисел: ");
+int[,] arr1 = new int[3, 3];
+int[,] arr2 = new int[3, 3];
+int[,] resultArray = new int[3, 3];
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        Random rnd = new Random();
+        arr1[i, j] = rnd.Next(1, 10);
+        Console.Write(arr1[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        Random rnd = new Random();
+        arr2[i, j] = rnd.Next(1, 10);
+        Console.Write(arr2[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine("Результат: ");
+Console.WriteLine();
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+
+        for (int k = 0; k < 3; k++)
+        {
+            resultArray[i, j] += (arr1[i, k] * arr2[k, j]);
+        }
+
+        Console.Write(resultArray[i, j] + " ");
+        resultArray[i, j] = 0;
+
+    }
+    Console.WriteLine();
+}
+
+
+
+
+
+/*
+Задача 3: Задайте две квадратные матрицы одинакового размера. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица:
+18 20
+15 18
+*/
